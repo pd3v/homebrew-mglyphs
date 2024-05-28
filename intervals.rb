3e2fc0a486030539e503cadb2485afe40129d5d8
pd3v <paulo.develop@gmail.com>
2024-05-28 16:56:50 +0100

    intervals formula

--- intervals.rb
+++ intervals.rb
@@ -1,7 +1,8 @@
 class Intervals < Formula
   desc "Notes, intervals, chords, scales and MIDI matchings"
   homepage "https://github.com/pd3v/intervals"
-  url "https://github.com/pd3v/intervals/archive/refs/tags/v0.0.1.tar.gz"
+  # url "https://github.com/pd3v/intervals/archive/refs/tags/v0.0.1.tar.gz"
+  head "https://github.com/pd3v/intervals.git"
   sha256 "4ed19c9ab1911b5d0ad99f524c08078444949e49814bb07a158febacd653f8d1"
   license "MIT"
 
