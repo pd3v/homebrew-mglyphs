43738061c1a121b450d2a966215466f4e7841547
pd3v <paulo.develop@gmail.com>
2024-05-28 16:18:51 +0100

    intervals formula

--- intervals.rb
+++ intervals.rb
@@ -10,9 +10,9 @@ class Intervals < Formula
   def install
     #system "./configure", "--disable-silent-rules", *std_configure_args
     system "mkdir", "build"
-    system "cd", "build"
+    #system "cd", "build"
     system "cmake", "-S", ".", "-B", "build/", *std_cmake_args
-    system "make", "-C", "/build" 
+    system "make", "-C", "build/" 
     bin.install "intervals"
   end
 end
