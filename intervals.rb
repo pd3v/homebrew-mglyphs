8f823819dc55b08c5de380f3325d763c71efff2b
pd3v <paulo.develop@gmail.com>
2024-05-29 17:46:29 +0100

    set url's tag and revision

--- intervals.rb
+++ intervals.rb
@@ -2,7 +2,9 @@ class Intervals < Formula
   desc "Notes, intervals, chords, scales and MIDI matchings"
   homepage "https://github.com/pd3v/intervals"
   # url "https://github.com/pd3v/intervals/archive/refs/tags/v0.0.1.tar.gz"
-  head "https://github.com/pd3v/intervals.git", :using => :git, :branch => :main
+  url "https://github.com/pd3v/intervals.git", #, :using => :git, :branch => :main
+    tag: "v0.6.2",
+    revision: "a49d7140503004682a7e5398804591ac9b697789"
   #sha256 "4ed19c9ab1911b5d0ad99f524c08078444949e49814bb07a158febacd653f8d1"
   #license "MIT"
 
