d055a6dd342a43f36d9691862dea064d42d8255e
pd3v <paulo.develop@gmail.com>
2024-05-29 18:20:12 +0100

    rename project

--- mglyphs.rb
+++ mglyphs.rb
@@ -1,10 +1,10 @@
-class Intervals < Formula
+class Mglyphs < Formula
   desc "Notes, intervals, chords, scales and MIDI matchings"
-  homepage "https://github.com/pd3v/intervals"
+  homepage "https://github.com/pd3v/mglyphs"
   # url "https://github.com/pd3v/intervals/archive/refs/tags/v0.0.1.tar.gz"
-  url "https://github.com/pd3v/intervals.git", #, :using => :git, :branch => :main
+  url "https://github.com/pd3v/mglyphs.git", #, :using => :git, :branch => :main
     tag: "v0.0.1",
-    revision: "8673da324b362878e627399fe392356a7a179c7d"
+    revision: "83808b98ffd224b55c91401984e4200333e39e10"
   #sha256 "4ed19c9ab1911b5d0ad99f524c08078444949e49814bb07a158febacd653f8d1"
   #license "MIT"
 
@@ -16,6 +16,6 @@ class Intervals < Formula
     #system "cd", "build"
     system "cmake", "-S", ".", "-B", "build/", *std_cmake_args
     system "make", "-C", "build/" 
-    bin.install "build/intervals"
+    bin.install "build/mglyphs"
   end
 end
