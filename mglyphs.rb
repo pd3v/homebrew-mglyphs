493b9511e1ebea7b1668dbc4f189d52a28953a9f
pd3v <paulo.develop@gmail.com>
2024-06-03 17:06:48 +0100

    dealing with lib loading

--- mglyphs.rb
+++ mglyphs.rb
@@ -3,7 +3,7 @@ class Mglyphs < Formula
   homepage "https://github.com/pd3v/mglyphs"
   # url "https://github.com/pd3v/intervals/archive/refs/tags/v0.0.1.tar.gz"
   url "https://github.com/pd3v/mglyphs.git", #, :using => :git, :branch => :main
-    tag: "v0.0.1",
+    tag: "v0.0.2",
     revision: "83808b98ffd224b55c91401984e4200333e39e10"
   #sha256 "4ed19c9ab1911b5d0ad99f524c08078444949e49814bb07a158febacd653f8d1"
   #license "MIT"
