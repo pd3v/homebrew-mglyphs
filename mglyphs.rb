b38ca6e93eb4702163a0abeec1ec63c9a8bdb17f
pd3v <paulo.develop@gmail.com>
2024-06-24 17:21:07 +0100

    comment out revision

--- mglyphs.rb
+++ mglyphs.rb
@@ -5,8 +5,8 @@ class Mglyphs < Formula
   url "https://github.com/pd3v/mglyphs.git", #, :using => :git, :branch => :main
   #tag: "v0.0.2",
   #revision: "493b9511e1ebea7b1668dbc4f189d52a28953a9f"
-  tag: "v0.0.1",
-  revision: "83808b98ffd224b55c91401984e4200333e39e10"
+  tag: "v0.0.1"
+  #revision: "83808b98ffd224b55c91401984e4200333e39e10"
   #revision: "2feb98a86d66d0d762c58d0b3e106b7b0adfa679"
 
   #sha256 "4ed19c9ab1911b5d0ad99f524c08078444949e49814bb07a158febacd653f8d1"
